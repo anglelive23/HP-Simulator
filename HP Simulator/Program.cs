1028fc2e58c0dff70f1b6167be7b544382d2ce46
Ahmed Allam <anglelive23@gmail.com>
2022-12-13 16:48:54 +0200

    Improving Attack Class

--- HP Simulator/Program.cs
+++ HP Simulator/Program.cs
@@ -6,8 +6,7 @@ namespace HP_Simulator
     {
         static void Main(string[] args)
         {
-            Attacks attacks = new Attacks(true); // true means Register Initial Attacks
-            Simulator simulator = new Simulator(attacks); // Simulate the game with Registered attacks
+            Simulator simulator = new Simulator(); // Simulate the game with Registered attacks
         }
     }
 }
